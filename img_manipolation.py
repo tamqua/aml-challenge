b0c84e74998a548dd8edbae079fe849f0472eb75
filvi <fil.vicari@gmail.com>
2021-05-11 01:50:32 +0200

    huge code refactoring :fire:, performance testing, processed folder of manipulated image creation and update readme

--- img_manipolation.py
+++ img_manipolation.py
@@ -5,26 +5,12 @@ import cv2
 import random
 from numba import prange, jit, parfors
 import matplotlib.pyplot as plt
+import os
 
 
 # %%
 # creating the generator for each image subset
 # ==================================================================
-
-
-
-# %%
-# debug image
-img = cv2.imread("dataset\\training\\1\\ec50k_00010002.jpg", cv2.IMREAD_COLOR)
-# %%
-
-# ==============================================================================
-# The jit function decorator of the packet Numba allows to compile the code to 
-# make it run faster. 
-
-# Normal -> 18.3 µs ± 319 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
-# Numba compiled -> 27.2 µs ± 733 ns per loop (mean ± std. dev. of 7 runs, 10000 loops each)
-# ==============================================================================
 def pick_color_channel(image, channel):
 
     """
@@ -49,17 +35,12 @@ def pick_color_channel(image, channel):
         return np.stack((img[:,:,2],)*np.uint8(3), axis=2)
     else:
         raise ValueError("The channel specified in `pick_color_channel(image, channel)` is incorrect.\nPlease use one of the following: `r` `g` `b`")
+# ==============================================================================
 
-    # else:
-    #     print(f"The value choosen was invalid")
-    #     return wrong
 
 
 
 # %%
-
-
-
 # ==============================================================================
 # The jit function decorator of the packet Numba allows to compile the code to 
 # make it run faster. 
@@ -78,7 +59,7 @@ def pick_color_channel(image, channel):
 #
 # Normal -> Wall time: 128 ms
 # Numba compile run ->  Wall time: 1.2 s
-# Numba compiled ->  Wall time: 2.s97 ms
+# Numba compiled ->  Wall time: 2.97 ms
 # ==============================================================================
 @jit(nogil=True, parallel=True, nopython=True)
 def noise_over_image(image, prob=0.01):
@@ -94,10 +75,12 @@ def noise_over_image(image, prob=0.01):
     # initialize the matrix which will be returned
     # --------------------------------------------------------------------------
     output = np.zeros(img.shape, np.uint8)
+    # --------------------------------------------------------------------------
     
     # set the max treshold for white value
     # --------------------------------------------------------------------------
     treshold = 1 - prob        
+    # --------------------------------------------------------------------------
 
     # for each row and column of the matrix check if the condition applies to
     # change the value to white or black pixel to create a salt and pepper noise
@@ -111,35 +94,50 @@ def noise_over_image(image, prob=0.01):
                 output[i][j] = 255
             else:
                 output[i][j] = img[i][j]
+    # --------------------------------------------------------------------------
 
     return output
+# ==============================================================================
+
+
 # %%
-
-
+# Fake HDR is a function that normalize the pixel from the alpha value to the beta
+# one. We have some handy preset if we don't want to specify these value separately
+# ==============================================================================
 def fakehdr(image, alpha=-100, beta=355, preset=None):
     img = image.copy()
     
+    # Setting the alpha and beta value for the presets
+    # --------------------------------------------------------------------------
     if preset == "dark":
-        alpha = -500
-        beta = 255
-    if preset == "light":
+        alpha = -100
+        beta = 150
+    elif preset == "light":
         alpha = 500
         beta = -100
     norm_img = cv2.normalize(img,None, alpha=alpha, beta=beta, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+    # --------------------------------------------------------------------------
+
     return norm_img
+# ==============================================================================
+
+
+
 # %%
-
-
-def visual_fakehdr_debug(imgpath, alpha=-100, beta=355, preset=None):
-    cv2.imshow('image',fakehdr(imgpath, alpha, beta, preset))
+# A simple visual debugger to test the correct value or preset with an image
+# ==============================================================================
+def visual_fakehdr_debug(image, alpha=-100, beta=355, preset=None):
+    cv2.imshow('image',fakehdr(image, alpha, beta, preset))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+# ==============================================================================
 
 
-# visual_fakehdr_debug(img, preset="light")
 
-
-def channel_vd(image):
+# %%
+# This function allows us to visually debug the changes in the images
+# ==============================================================================
+def global_visual_debugger(image, savefig=False, fname=0):
     """
     VD stands for visual debug, with this function we can have visual
     information about the changes made in the images while applying the various
@@ -149,118 +147,152 @@ def channel_vd(image):
 
     
     # subplot settings
-    # ==========================================================================
+    # --------------------------------------------------------------------------
     column = 4
     row = 3
     fig=plt.figure(dpi=300)
     fig.subplots_adjust(wspace = 0.5, hspace = 0.5)
+    # --------------------------------------------------------------------------
+    
     
     # Original
-    # ==========================================================================
+    # --------------------------------------------------------------------------
     img_original = img
     im1 = fig.add_subplot(row,column,1)
     im1.title.set_text("original image")
     im1.title.set_size(5)
     plt.axis('off')
     plt.imshow(img)
-    
+    plt.close()
+    # --------------------------------------------------------------------------
+
+
+    # Original + Noise
+    # --------------------------------------------------------------------------
     noise_original = noise_over_image(img)
     im2 = fig.add_subplot(row,column,5)
     im2.title.set_text("original image + noise")
     im2.title.set_size(5)
     plt.axis('off')
     plt.imshow(noise_original)
+    plt.close()
+    # --------------------------------------------------------------------------
 
 
     # Red 
-    # ==========================================================================
+    # --------------------------------------------------------------------------
     img_red = pick_color_channel(img, "r")
     im3 = fig.add_subplot(row,column,2)
     im3.title.set_text("red channel")
     im3.title.set_size(5)
     plt.axis('off')
     plt.imshow(img_red)
+    plt.close()
+    # --------------------------------------------------------------------------
 
+    # Red + Noise
+    # --------------------------------------------------------------------------
     noise_red = noise_over_image(img_red)
     im4 = fig.add_subplot(row,column,6)
     im4.title.set_text("red + noise")
     im4.title.set_size(5)
     plt.axis('off')
     plt.imshow(noise_red)
+    plt.close()
+    # --------------------------------------------------------------------------
 
     # Green
-    # ==========================================================================
+    # --------------------------------------------------------------------------
     img_green = pick_color_channel(img, "g")
     im5 = fig.add_subplot(row,column,3)
     im5.title.set_text("green channel")
     im5.title.set_size(5)
     plt.axis('off')
     plt.imshow(img_green)
+    plt.close()
+    # --------------------------------------------------------------------------
 
+    # Green + Noise
+    # --------------------------------------------------------------------------
     noise_green = noise_over_image(img_green, 0.01)
     im6 = fig.add_subplot(row,column,7)
     im6.title.set_text("green channel + noise")
     im6.title.set_size(5)
     plt.axis('off')
     plt.imshow(noise_green)
+    plt.close()
+    # --------------------------------------------------------------------------
 
     # Blue
-    # ==========================================================================
+    # --------------------------------------------------------------------------
     img_blue = pick_color_channel(img, "g")
     im7 = fig.add_subplot(row,column,4)
     im7.title.set_text("blue channel")
     im7.title.set_size(5)
     plt.axis('off')
     plt.imshow(img_blue)
+    plt.close()
+    # --------------------------------------------------------------------------
 
+    # Blue + Noise
+    # --------------------------------------------------------------------------
     noise_blue = noise_over_image(img_blue, 0.013)
     im8 = fig.add_subplot(row,column,8)
     im8.title.set_text("blue channel + noise")
     im8.title.set_size(5)
     plt.axis('off')
     plt.imshow(noise_blue)
+    plt.close()
+    # --------------------------------------------------------------------------
     
-    # Fake HDR preset dark
-    # ==========================================================================
+    # Fake HDR preset DARK
+    # --------------------------------------------------------------------------
     img_hdr_dark = fakehdr(img, preset="dark")
     im9 = fig.add_subplot(row,column,9)
     im9.title.set_text("Fake HDR dark")
     im9.title.set_size(5)
     plt.axis('off')
     plt.imshow(img_hdr_dark)
+    plt.close()
+    # --------------------------------------------------------------------------
 
+    # Fake HDR preset DARK + Noise
+    # --------------------------------------------------------------------------
     noise_hdr_dark = noise_over_image(img_hdr_dark, 0.013)
     im10 = fig.add_subplot(row,column,10)
     im10.title.set_text("Fake HDR dark + noise")
     im10.title.set_size(5)
     plt.axis('off')
     plt.imshow(noise_hdr_dark)
+    plt.close()
+    # --------------------------------------------------------------------------
     
     
-    # Fake HDR preset light
-    # ==========================================================================
+    # Fake HDR preset LIGHT
+    # --------------------------------------------------------------------------
     img_hdr_light = fakehdr(img, preset="light")
     im11 = fig.add_subplot(row,column,11)
     im11.title.set_text("Fake HDR light")
     im11.title.set_size(5)
     plt.axis('off')
     plt.imshow(img_hdr_light)
+    plt.close()
+    # --------------------------------------------------------------------------
 
+
+    # Fake HDR preset LIGHT + Noise
+    # --------------------------------------------------------------------------
     noise_hdr_light = noise_over_image(img_hdr_light, 0.013)
     im12 = fig.add_subplot(row,column,12)
     im12.title.set_text("Fake HDR Light + noise")
     im12.title.set_size(5)
     plt.axis('off')
     plt.imshow(noise_hdr_light)
-
-    plt.show()
-
-
-channel_vd(img)
-
-# %%
-
-# %%
-
-
-# %%
+    plt.close()
+    # --------------------------------------------------------------------------
+    if not savefig:
+        plt.show()
+    else:
+        plt.savefig(fname)
+    plt.close("all")
+# ==============================================================================
--- parse_images.py
+++ parse_images.py
@@ -2,14 +2,43 @@
 import os
 from numba import jit, prange
 from pprint import pprint
+import cv2
+from img_manipolation import *
+from tqdm import tqdm
+import logging
+logging.basicConfig(filename='parse.log', encoding='utf-8', level=logging.INFO)
+# %%
 # initialize the generator for the respective folders
 # ==============================================================================
-training_path =  os.walk(os.path.join("dataset", "training"), topdown=False)
-validation_gallery_path =  os.walk(os.path.join("dataset", "validation", "gallery"), topdown=False)
-validation_query_path =  os.walk(os.path.join("dataset", "validation", "query"), topdown=False)
+training_path           = os.walk(os.path.join("dataset", "training"), topdown=False)
+validation_gallery_path = os.walk(os.path.join("dataset", "validation", "gallery"), topdown=False)
+validation_query_path   = os.walk(os.path.join("dataset", "validation", "query"), topdown=False)
 
-# TODO inventarsi un nome migliore per la classe
-class Item:
+
+# %%
+# Create the class for the Training and Validation instances
+# ==============================================================================
+class Dataset:
+    """
+    The class Dataset is standardize all the tasks easily across The Training
+    and Test set. 
+    The methods are:
+        print_dirs()    -> perform pprint of the directory saved in self.list_dirs
+        get_dirs()      -> return (lst) self.list_dirs
+        len_dirs()      -> return (int) len(self.list_dirs)
+        print_files()   -> perform pprint of the files saved in self.list_files
+        get_files()     -> return (lst) self.list_files
+        len_files()     -> return (int) len(self.list_files)
+        parse_image()   -> return a generator for looping through the dataset
+    """
+
+    # On instance creation parse the directory and add them to a list
+    # Although we have nested loops and Big-OH Notation of N^2 the average time 
+    # for the given dataset is pretty reasonable, as follows:
+    # training_path:            277 ns ± 12.7 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+    # validation_gallery_path:  274 ns ± 9.12 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+    # validation_query_path:    269 ns ± 10.9 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+    # --------------------------------------------------------------------------
     def __init__(self, mygenerator):
         self.list_dirs = []
         self.list_files = []
@@ -18,35 +47,166 @@ class Item:
             for d in prange(len(dirs)):
                 self.list_dirs.append(os.path.join(root, dirs[d]))
             for f in  prange(len(files)):
-                self.list_files.append(os.path.join(root, files[f]))
+                if not files[f].endswith('.DS_Store'):
+                    self.list_files.append(os.path.join(root, files[f]))
+    # --------------------------------------------------------------------------
 
+
+    # Utilities functions on the directory list
+    # --------------------------------------------------------------------------
     def print_dirs(self):
         pprint(self.list_dirs)
+
     def get_dirs(self):
         return self.list_dirs
+
+    def len_dirs(self):
+        return len(self.list_dirs)
+    # --------------------------------------------------------------------------
     
+    
+    # Utilities functions on the file list
+    # --------------------------------------------------------------------------
     def print_files(self):
         pprint(self.list_files)
+
     def get_files(self):
         return self.list_files
+
+    def len_files(self):
+        return len(self.list_files)
+    # --------------------------------------------------------------------------
+
+
+    # creating a generator to use later on with all the image loaded
+    # --------------------------------------------------------------------------
+    def parse_image(self, color=False):
+        for i in prange(len(self.list_files)):
+            img = cv2.imread(self.list_files[i], cv2.IMREAD_COLOR)
+            yield img
+    # --------------------------------------------------------------------------
+
+
+
 # %%
 
+# initialize the instance of the Dataset Class
+# ==============================================================================
+Training            = Dataset(training_path)
+Validation_Gallery   = Dataset(validation_gallery_path)
+Validation_Query    = Dataset(validation_query_path)
+# ==============================================================================
 
-Training = Item(training_path)
-Validation_Galery = Item(validation_gallery_path)
-Validation_Query = Item(validation_query_path)
 
 
 # %%
+# Debug pprint all files
+# ==============================================================================
+# Training.print_files()
+# Validation_Gallery.print_files()
+# Validation_Query.print_files()
+# ==============================================================================
 
-# Debug pprint all files
+
+# Debug pprint all directories
 # ==============================================================================
-Training.print_files()
-Validation_Galery.print_files()
-Validation_Query.print_files()
-# Debug pprint all files
+# Training.print_dirs()
+# Validation_Gallery.print_dirs()
+# Validation_Query.print_dirs()
 # ==============================================================================
-Training.print_dirs()
-Validation_Galery.print_dirs()
-Validation_Query.print_dirs()
+
+
+
+# %%
+# Initial folder setup, create the folder structure to save the altered images
+# ==============================================================================
+def create_dir_structure(basedir, subdir=None, sub_subdir=None):
+    if not os.path.isdir(basedir):
+        os.mkdir(basedir)
+    if subdir != None:
+        if not os.path.isdir(os.path.join(basedir, subdir)):
+            os.mkdir(os.path.join(basedir, subdir))
+    if subdir != None and sub_subdir != None:
+        if not os.path.isdir(os.path.join(basedir, subdir, sub_subdir)):
+            os.mkdir(os.path.join(basedir, subdir, sub_subdir))
+# ==============================================================================
+
+
+# %%
+# Create basic structure for storing the altered images for debug purpose
+# ==============================================================================
+def create_processed():
+    create_dir_structure("processed")
+    create_dir_structure("processed", "training")
+    create_dir_structure("processed", "validation")
+    create_dir_structure("processed", "validation", "gallery")
+    create_dir_structure("processed", "validation", "query")
+# ==============================================================================
+
+
+# example usage of the generator in file saving mode
+# ==============================================================================
+def save_all_images(myinstance):
+    counter = 0
+    failed = 0
+    all_files = myinstance.get_files()
+    for img in tqdm(myinstance.parse_image(color=False), total=myinstance.len_files()):
+        
+        # extrapolate from the filename the new path replacing the folder name
+        # --------------------------------------------------------------------------
+        fname = all_files[counter].replace("dataset", "processed")
+        # --------------------------------------------------------------------------
+        # if the file exist skips incrementing the loop
+        # --------------------------------------------------------------------------
+        if os.path.isfile(fname):
+            counter += 1
+            continue
+        # --------------------------------------------------------------------------
+
+
+        # The algorithm tries to save the image, if no subfolder is found it creates
+        # one and then try again to save the image
+        # --------------------------------------------------------------------------
+        try:
+            global_visual_debugger(img, savefig=True, fname=fname)
+            logging.info(f'created visual debug for {fname}')
+        except:
+            sep_index = fname.rfind(os.path.sep)
+            splitted_fname = fname[sep_index:]
+            fpath = fname[:sep_index]
+            os.mkdir(fpath)
+            try:
+                global_visual_debugger(img, savefig=True, fname=os.path.join(fpath,splitted_fname))
+                logging.info(f'created folder {fpath}')
+            except:
+                logging.warning(f'Failed to save {splitted_fname} in {fpath}')
+                failed +=1
+        # --------------------------------------------------------------------------
+
+        counter += 1
+    print(f"Process finished with {failed} operations, you can run again or take a look at parse.log file")
+# ==============================================================================
+
+
+
+# %%
+# example usage of the generator in file viewing mode
+# ==============================================================================
+def visualize_all_images():
+    counter = 0
+    all_files = Training.get_files()
+    for img in tqdm(Training.parse_image(color=False), total=Training.len_files()):
+        # pick_color_channel(img, "r")
+        # noise_over_image(img, prob=0.015)
+        # fakehdr(img, alpha=-100, beta=355, preset=None)
+        # visual_fakehdr_debug(img, preset="dark")
+        # visual_fakehdr_debug(img, alpha=-100, beta=355)
+        # global_visual_debugger(img)
+
+        # global_visual_debugger(img)
+
+        pass
+# ==============================================================================
+
+
 # %%
